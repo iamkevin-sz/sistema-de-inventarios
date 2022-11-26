e186145c73b01426766c5a2806f24fcc52038552
kevin_sz <iamkevinnumber1@gmail.com>
2022-11-26 12:10:32 -0400

    categoria

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,24 +1,89 @@
-@extends('layouts.admin')
-@section('title','Gestión de categorías')
-@section('styles')
-{!! Html::style('treegrid/css/jquery.treegrid.css') !!}
-@endsection
-@section('options')
-@endsection
-@section('preference')
-@endsection
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Lista de Categorias</h1>
+@stop
+
 @section('content')
-<div class="content-wrapper">
 
-</div>
-@endsection
-@section('scripts')
+    <div class="card">
+        <div class="card-header">
 
-{!! Html::script('treegrid/js/jquery.treegrid.js') !!}
-{!! Html::script('js/my_functions.js') !!}
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('.tree').treegrid().treegrid('collapseAll');
+            <a href="{{route('admin.categories.create')}}"> Agregar Categorias </a>
+
+        </div>
+        <div class="card-body">
+            <table id="data-tables" class="table table-striped">
+                <thead align="center">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody align="center">
+                    @foreach ($categories as $category )
+
+
+                    <tr>
+                        <td>{{$category -> id}}</td>
+                        <td>{{$category -> name}}</td>
+                        <td>{{$category -> description}}</td>
+
+
+
+                        <td width="10px" style="display: inline-flex">
+                            <a class="btn btn-secondary" href="{{route('admin.categories.edit', $category)}}">Editar</a>&nbsp;&nbsp;&nbsp;
+
+                            <form action="{{route('admin.categories.destroy', $category)}}" method="POST">
+                            @method('delete')
+                            @csrf
+
+                            <button class="btn btn-danger" type="submit">Eliminar</button>
+
+                            </form>
+                        </td>
+
+                        {{-- {!! Form::open(['route' => 'admin.categories.destroy']) !!} --}}
+                    </tr>
+
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    $('#data-tables').DataTable({
+        "language": {
+            "search":       "Buscar",
+            "lengthMenu":   "Mostrar _MENU_ registros por página",
+            "info":         "Mostrando página _PAGE_ de _PAGES_",
+            "zeroRecords": 'No se encontró lo que buscas',
+            "infoEmpty": '0 resultados',
+            "infoFiltered": '(Filtrado de _MAX_ registros totales)',
+            "paginate": {
+
+                            "previous": "Anterior",
+                            "next": "Siguiente",
+                            "first": "Primero",
+                            "last": "Último"
+            }
+        }
     });
-</script>
-@endsection
+    </script>
+@stop
+
+
+
